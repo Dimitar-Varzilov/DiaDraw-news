d153d6e549151f670a731bee2e4f4c00b224eaf9
Dimitar Varzilov <dnvarzilov@diadraw.com>
2023-06-29 17:34:31 +0300

    Revert "footer not showing at the bottom"
    This reverts commit eab527d2b8274019737823ce7ccd9e09770162d4.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@material-tailwind/react";
 
 export default function Example() {
   return (
-    <footer className="via-sky-500  to-emerald-500 bottom-0 left-0 w-full bg-white bg-gradient-to-t from-indigo-500 from-10% via-30% to-90% p-7">
+    <footer className="via-sky-500  to-emerald-500 absolute  bottom-0 left-0 w-full bg-white bg-gradient-to-t from-indigo-500 from-10% via-30% to-90% p-7">
       <Typography color="blue-gray" className="text-center font-normal">
         &copy; {new Date().getFullYear()} DiaDraw. All Rights Reserved.
       </Typography>
