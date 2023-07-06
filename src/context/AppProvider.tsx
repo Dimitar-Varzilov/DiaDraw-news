ab9b4092765f6a78f73a47da1c6bc2c98c38af98
DimitarVarzilov <dnvarzilov@diadraw.com>
2023-07-06 13:25:01 +0300

    getNewsById and getRelatedNews methods

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -12,11 +12,22 @@ export interface IAppState {
   news: INews[];
   createNews: (baseNews: IBaseNews) => INews;
   addNews: (news: INews) => void;
+  getNewsById: (newsId: string) => INews | undefined;
+  getRelatedNews: (
+    id: string,
+    options?: {
+      resultsCount?: number;
+      includeSelf?: boolean;
+    }
+  ) => INews[];
 }
+
 const initialState: IAppState = {
   news: [],
   addNews: (news: INews) => news,
   createNews,
+  getNewsById: () => undefined,
+  getRelatedNews: () => [],
 };
 
 const AppContext = createContext<IAppState>(initialState);
@@ -25,6 +36,7 @@ export const useAppContext = (): IAppState => useContext(AppContext);
 
 const AppContextProvider = ({ children }: AppProviderProps) => {
   const [news, setNews] = useState<INews[]>(exampleNews);
+
   const addNews = (newNews: INews) => {
     setNews((prev) => {
       return [...prev, newNews].sort(
@@ -32,8 +44,44 @@ const AppContextProvider = ({ children }: AppProviderProps) => {
       );
     });
   };
+
+  const getNewsById = (newsId: string) => {
+    return news.find(({ id }) => id === newsId);
+  };
+
+  const getRelatedNews = (
+    id: string,
+    options?: {
+      resultsCount?: number;
+      includeSelf?: boolean;
+    }
+  ): INews[] => {
+    const newsFound = getNewsById(id);
+    let results: INews[] = [];
+    if (!newsFound) return results;
+    if (options) {
+      const { resultsCount, includeSelf } = options;
+      results = news.filter(({ id, type }) => {
+        if (includeSelf) {
+          return type === newsFound.type || id === newsFound.id;
+        }
+        return type === newsFound.type && id !== newsFound.id;
+      });
+      if (resultsCount) return results.slice(0, resultsCount);
+    }
+    return results;
+  };
+
   return (
-    <AppContext.Provider value={{ news, addNews, createNews }}>
+    <AppContext.Provider
+      value={{
+        news,
+        addNews,
+        createNews,
+        getNewsById,
+        getRelatedNews,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
